3d1a87922cd4ee888855d45034b35113f20c3061
Abrahán Mesa <ab.mesa@icloud.com>
2021-02-04 16:07:12 +0100

    vue router - receiving url params - building pagination - nested routes

---
+++ vue3/router/src/router/index.js
@@ -0,0 +1,52 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventList from '../views/EventList.vue'
+import EventDetails from '../views/event/Details.vue'
+import EventRegister from '../views/event/Register'
+import EventEdit from '../views/event/Edit'
+import EventLayout from '../views/event/Layout'
+
+import About from '../views/About.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'EventList',
+    component: EventList,
+    props: route => ({ page: parseInt(route.query.page) || 1 })
+  },
+  {
+    path: '/event/:id',
+    name: 'EventLayout',
+    props: true,
+    component: EventLayout,
+    children: [
+      {
+        path: '',
+        name: 'EventDetails',
+        component: EventDetails
+      },
+      {
+        path: 'register',
+        name: 'EventRegister',
+        component: EventRegister
+      },
+      {
+        path: 'edit',
+        name: 'EventEdit',
+        component: EventEdit
+      },
+    ]
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
