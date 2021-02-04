8ebf050a7e943e6bfda09f81f9cf9a845d92cb8e
Abrahán Mesa <ab.mesa@icloud.com>
2021-02-04 16:29:22 +0100

    vue router - programmatic navigation

--- vue3/router/src/views/event/Register.vue
+++ vue3/router/src/views/event/Register.vue
@@ -1,9 +1,21 @@
 <template>
-  <p>Register for the event here</p>
+  <p>Registration form here</p>
+  <button @click="register">Register Me!</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  methods: {
+    register() {
+      // If registration API call is successful
+      // Push back to the event details
+      this.$router.push({
+        name: 'EventDetails',
+        // we can omit this line as the id is expected (router index.js) and it will be injected by default
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
